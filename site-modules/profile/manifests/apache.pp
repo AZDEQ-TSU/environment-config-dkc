ceacbb584e6f4f0345f6dd32ab54ed97760eb459
AZDEQ-TSU <51378580+AZDEQ-TSU@users.noreply.github.com>
2019-07-12 13:29:47 -0700

    Added index.html

--- site-modules/profile/manifests/apache.pp
+++ site-modules/profile/manifests/apache.pp
@@ -16,10 +16,18 @@ class profile::apache {
     ensure => 'present'
   }
   file { '/var/www/html/phpinfo.php':
+    notify  => Service['httpd'],
     ensure  => 'present',
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
     content => "<?php\nphpinfo();\n?>"
+  }
+  file { '/var/www/html/index.html':
+    ensure => 'present',
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    content => "<html>\n<a href=phpinfo.php>PHP Information</a>\n</html>"
   }
 }
