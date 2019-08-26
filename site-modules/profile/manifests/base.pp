bf535f8ccd56be85e11f1b748e83bdab0bd3df14
AZDEQ-TSU <51378580+AZDEQ-TSU@users.noreply.github.com>
2019-08-26 15:20:46 -0700

    Escaped sshkey long line

--- site-modules/profile/manifests/base.pp
+++ site-modules/profile/manifests/base.pp
@@ -31,7 +31,23 @@ class profile::base {
     ensure => present,
     user   => 'dcrowfo',
     type   => 'ssh-rsa',
-    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAAEAQC80IFhgyL7wLI7fsAj82q3Kx9JgS/kjjHDaVyODraxATVuKGyeNtfvJ2nTHb/HAriB8mghAzhgtL7OPW3pVEwtaRIlnrLeQ8gxSBjipGYZNUBXISYzYHQRi7Wxgw4C4vj120kpwVNcIyzyItjGyE0znlEBaYB4JLZq4JBoVIlnto85dm6h7l85Ig1hG2V4ost1PhYGaJrooIbcsO3u0k7P//xkiTGDk2drMmu/ralU+i3OXhJPExWvw6uPdRVci5HVW2fslb5Y6cFOC80+BDDloOnOOjDGhJYPVi/mncMBszJbRTVfCQvBHMr4RbvsZRmf2ikKv46qzuReco7mu1uL7HtRhSc718PyiAEk5H3BoB7rk2qDqc8rAmef298m7XTH7cBQpPu6onS0SOPyXIPn6eI3iniSaysE3r2sTxWyDWg20l6/cCTwEu0dU6c1K4FUQDw2892mz9xGSJY9JcfKvBa0FujdM+kiIuV49iCalJaU13nhCkMAft5VFKTDRsojvxZbhYZCX6PbqkD72AYhdrYPaq1cTG22ARaAaiGjqOVxVCz6UG7mtT9vL4iyH+KRZgxIOctZ0gYaPPccAlGwj/Vj7D0olZHfjnWo+dditoq8QUpHsaYbs4Uioot8oex1X2T7MW7PRdi+OgwZhhyPVuyUvxXiqG4J2losoY9p5HB1jEN9t/7+++QNHYsKZeUeWHRC4CTx8ZzRshv9jlLvk7dWdQq2uVk/GxIHCqfoImD8ggsqIF6Gdriia+ITW7Re72j1AwFk0lA1+ykYPU+Q05itOjB/mKS0R0GkeIVISmCXWUgrhWIb61Ep28CQNKqDVZ8qB2ijcTTsqYok//nxuCl3eFz/ScfnYjpyuk4slk/fuxyAFldunpA1pu66jLRmlKoKA5wKtAyqhyVVqyS/8xrLeHfDvbjBvcKVcZrx55dATJIL4Jgn68EesFPeKdU/udwy7SAs5Kx3v87Z3x6XEs65q/FBGHU+77rjNNA8qx2ZTJQ4FbroPL/kchyxi4fy5V7d35r1iuWn944nEzK8FfONCFiiyd/El6haA2neqeZMe/4dNrDX9Xo/XwHfpj6MmVN4ZAr8DD/fMB7TZAsollJum1dFJ/KYGjn2zUMjwJYPoOGno2rQ0bV5XtPY4btozN19uMcPpulgc/Sqabw59IG58LNYdX4XlZM0V4ghk7wHno0rv5pPwsy1DBQB1gZxCNQEa8ZzRbSGLa7dzQTerT9A5cuZLGdQnN9wWteTyxdKqunClxHh30302+Ij21/mijqWHrZA/BkGOZdEErieJxQbHOb31G1sd9J45i1+8As7wll+RJzMBzZD0gRTuENwisFbEfkfI4AcEACqRWab',
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAAEAQC80IFhgyL7wLI7fsAj82q3Kx9JgS/kjjHDaVyODraxATVuKGyeNtf\
+               vJ2nTHb/HAriB8mghAzhgtL7OPW3pVEwtaRIlnrLeQ8gxSBjipGYZNUBXISYzYHQRi7Wxgw4C4vj120kpwV\
+               NcIyzyItjGyE0znlEBaYB4JLZq4JBoVIlnto85dm6h7l85Ig1hG2V4ost1PhYGaJrooIbcsO3u0k7P//xki\
+               TGDk2drMmu/ralU+i3OXhJPExWvw6uPdRVci5HVW2fslb5Y6cFOC80+BDDloOnOOjDGhJYPVi/mncMBszJb\
+               RTVfCQvBHMr4RbvsZRmf2ikKv46qzuReco7mu1uL7HtRhSc718PyiAEk5H3BoB7rk2qDqc8rAmef298m7XT\
+               H7cBQpPu6onS0SOPyXIPn6eI3iniSaysE3r2sTxWyDWg20l6/cCTwEu0dU6c1K4FUQDw2892mz9xGSJY9Jc\
+               fKvBa0FujdM+kiIuV49iCalJaU13nhCkMAft5VFKTDRsojvxZbhYZCX6PbqkD72AYhdrYPaq1cTG22ARaAa\
+               iGjqOVxVCz6UG7mtT9vL4iyH+KRZgxIOctZ0gYaPPccAlGwj/Vj7D0olZHfjnWo+dditoq8QUpHsaYbs4Ui\
+               oot8oex1X2T7MW7PRdi+OgwZhhyPVuyUvxXiqG4J2losoY9p5HB1jEN9t/7+++QNHYsKZeUeWHRC4CTx8Zz\
+               Rshv9jlLvk7dWdQq2uVk/GxIHCqfoImD8ggsqIF6Gdriia+ITW7Re72j1AwFk0lA1+ykYPU+Q05itOjB/mK\
+               S0R0GkeIVISmCXWUgrhWIb61Ep28CQNKqDVZ8qB2ijcTTsqYok//nxuCl3eFz/ScfnYjpyuk4slk/fuxyAF\
+               ldunpA1pu66jLRmlKoKA5wKtAyqhyVVqyS/8xrLeHfDvbjBvcKVcZrx55dATJIL4Jgn68EesFPeKdU/udwy\
+               7SAs5Kx3v87Z3x6XEs65q/FBGHU+77rjNNA8qx2ZTJQ4FbroPL/kchyxi4fy5V7d35r1iuWn944nEzK8FfO\
+               NCFiiyd/El6haA2neqeZMe/4dNrDX9Xo/XwHfpj6MmVN4ZAr8DD/fMB7TZAsollJum1dFJ/KYGjn2zUMjwJ\
+               YPoOGno2rQ0bV5XtPY4btozN19uMcPpulgc/Sqabw59IG58LNYdX4XlZM0V4ghk7wHno0rv5pPwsy1DBQB1\
+               gZxCNQEa8ZzRbSGLa7dzQTerT9A5cuZLGdQnN9wWteTyxdKqunClxHh30302+Ij21/mijqWHrZA/BkGOZdE\
+               ErieJxQbHOb31G1sd9J45i1+8As7wll+RJzMBzZD0gRTuENwisFbEfkfI4AcEACqRWab',
   }
   sudo::conf { 'admins':
     priority => 10,
